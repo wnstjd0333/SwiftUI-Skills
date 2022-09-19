6c0d9c1aaab1381206bf7fabc6bb929c2f687ec7
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-09-19 14:08:45 +0900

    add project

---
+++ Shared/SwiftUI_SkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SkillsApp.swift
+//  Shared
+//
+//  Created by 金峻聖 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
