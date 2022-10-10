496c2bb54da81f5ce9a7e81070a8a1d33345643c
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-10-11 00:39:09 +0900

    add: scrollview reader

---
+++ Shared/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,59 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftUI-Skills
+//
+//  Created by 金峻聖 on 2022/10/11.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+                }
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReaderBootcamp()
+    }
+}
