f3938ace09e81b128757ba7d2d24156e83377ec6
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-10-25 22:24:15 +0900

    add: background threads

---
+++ Shared/BackgroundThreadBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftUI-Skills
+//
+//  Created by 金峻聖 on 2022/10/25.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchDate() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 1: \(Thread.current)")
+
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 2: \(Thread.isMainThread)")
+                print("CHECK 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchDate()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+struct BackgroundThreadBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThreadBootcamp()
+    }
+}
