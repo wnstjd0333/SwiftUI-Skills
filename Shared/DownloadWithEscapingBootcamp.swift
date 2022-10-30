2fc800874868633ddf0f39bb5e8e3139cf6f8e1c
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-10-30 13:07:59 +0900

    add: download with combine

--- Shared/DownloadWithEscapingBootcamp.swift
+++ Shared/DownloadWithEscapingBootcamp.swift
@@ -7,13 +7,6 @@
 
 import SwiftUI
 
-struct PostModel: Identifiable, Codable {
-    let userId: Int
-    let id: Int
-    let title: String
-    let body: String
-}
-
 class DownloadWithEscapingViewModel: ObservableObject {
     
     @Published var posts: [PostModel] = []
